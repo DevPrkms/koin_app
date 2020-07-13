683bae1fe4b428c89af395004abd6037dd3c42c4
DevPrkms <DevPrkms@gmail.com>
2020-07-13 09:53:04 +0900

    Splash Screen creating

---
+++ koin_project/android/app/src/main/kotlin/koinproject/koin_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package koinproject.koin_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ koin_project/lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:koin_project/homeScreen.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new SplashScreen(),
+    routes: <String, WidgetBuilder>{
+     '/HomeScreen': (BuildContext) => new HomeScreen()
+    },
+  ));
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 4);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Image.asset('images/coin.png'),
+      ),
+    );
+  }
+}
