2244d1b4d22f93bca8767482688424e452ccf912
DevPrkms <DevPrkms@gmail.com>
2020-07-14 11:19:14 +0900

    메인페이지 작업

--- koin_project/lib/homeScreen.dart
+++ koin_project/lib/homeScreen.dart
@@ -5,12 +5,17 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => new _HomeScreenState();
 }
 
+// appBar 삭제
+// BottomNavigatorBar 추가
+// 우측 전체메뉴 추가 ?
+// 메인 블록 디자인 추가
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('HomeScreen'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
       ),
       body: new Center(
         child: new Text('Welcom to Home!'),
